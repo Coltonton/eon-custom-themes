51a525e9e85a37ff0e7d2a813406539a9944667c
Shane Smiskol <shane@smiskol.com>
2020-08-26 08:57:45 -0500

    Clean up install_theme.py (#4)
    * make executable
    
    * rename to support
    
    * start to refactor into a class, use safer realpath(__file__)
    
    * move welcome to support
    
    * some more refactoring
    
    * more refactoring
    
    * change to theme_options
    
    * disable
    
    * refactor os.system calls to use format
    
    * and now refactor auto installer
    
    * move check_auto_installability to support file
    
    * fix check_auto_installability logic, it would previously keep auto installing
    
    * clean up imports
    
    * finish refactoring auto installer
    
    * clean up a bit
    
    * user safer reboot intent
    
    * create support_variables
    
    * remove old
    
    * sort the functions
    
    * final changes
    
    * final changes
    
    * test
    
    * fix logic
    
    * fix logic
    
    * fix logic
    
    * debug
    
    * debug
    
    * formatting
    
    * formatting
    
    * alert when theme has no resources for user's device
    
    * break with left arrow
    
    * break with left arrow
    
    * give time for statement to print
    
    * fix logic
    
    * make sure user wants to go through with the install
    
    * exit when rebooting
    
    * allow to be executable
    
    * move WELCOME_TEXT to vars
    
    * keep looping, don't go to menu
    
    * fix text
    
    * change text
    
    * change func name
    
    * Remove "Pick"
    
    
    Co-authored-by: Coltonton <41667601+Coltonton@users.noreply.github.com>

---
+++ support/support_variables.py
@@ -0,0 +1,15 @@
+CONTRIB_THEMES = 'contributed-themes'
+EXCLUDED_THEMES = ['Comma-Default', 'Example', 'ignoreme']
+MIN_SIM_THRESHOLD = 0.25  # user's input needs to be this percent or higher similar to a theme to select it
+
+# Auto Install variables - see DEVREADME
+IS_AUTO_INSTALL = False
+AUTO_INSTALL_CONF = {'selected_theme': 'arne', 'install_logo': False, 'install_anim': False,
+                     'install_spinner': False, 'openpilot_dir_name': 'arnepilot', 'install_additional': False}
+
+WELCOME_TEXT = ['Created By: Colton (Brandon) S. EndLine \\n',
+                'Special Thanks to @ShaneSmiskol for all the help!!!',
+                'Free to use! Free to Edit! Free to integrate!',
+                'Design and contribute your themes today!',
+                '(See the developer folder in this repo)',
+                'It\'s your EON, do what you want!']
