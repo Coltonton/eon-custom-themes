0d7cd18aba7815deb5038e36e110d1506edb34c1
Coltonton <41667601+Coltonton@users.noreply.github.com>
2020-09-01 15:31:37 -0400

    stupid collans

--- install_theme.py
+++ install_theme.py
@@ -152,7 +152,7 @@ class ThemeInstaller:
           continue
         
         theme_options = []
-        if path.exists('./contributed_themes/{}/colored_bootannimation.zip'.format(selected_theme)) 
+        if path.exists('./contributed_themes/{}/colored_bootannimation.zip'.format(selected_theme)): 
           color += 1
           colorOptions.append('Colored boot animation')
         if path.exists('./contributed_themes/{}/white_bootannimation.zip'.format(selected_theme)):
