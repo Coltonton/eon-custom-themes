6c53b3e6627dc98a99ddb8839da3cfaddf1992e4
Coltonton <41667601+Coltonton@users.noreply.github.com>
2020-09-18 03:04:45 -0400

    the b comes back the very next day

--- support/support_functions.py
+++ support/support_functions.py
@@ -1,8 +1,10 @@
 import os
 import time
 import difflib
-from support.support_variables import CONTRIB_THEMES, CURRENT_AUTO_VER, DO_NOT_AUTO_INSTALL, EXCLUDED_THEMES, MIN_SIM_THRESHOLD, WELCOME_TEXT, AUTO_WELCOME_TEXT
+from support.support_variables import CONTRIB_THEMES, EXCLUDED_THEMES, MIN_SIM_THRESHOLD, WELCOME_TEXT, AUTO_WELCOME_TEXT
 
+CURRENT_AUTO_VER = 1
+DO_NOT_AUTO_INSTALL = 0
 
 def check_auto_installability():
   AUTO_INSTALL_PATH = './support/auto_install_ver.txt'
