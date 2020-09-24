a0b21ef25f61d218a2efe2539dc5ba2994df41a9
Coltonton <41667601+Coltonton@users.noreply.github.com>
2020-09-24 12:24:46 -0400

    add backup reinstaller into main

--- install_theme.py
+++ install_theme.py
@@ -131,7 +131,7 @@ class ThemeInstaller:
         print('Didn\'t select a theme, exiting.')
         return
       if self.selected_theme == 'restore':
-        print('restore')
+        backup_reinstaller_loop()
       self.get_available_options()
       if self.install_function() == 'exit':
         return
@@ -334,8 +334,113 @@ class ThemeInstaller:
     fi = open("./support/auto_install_ver.txt", "w")
     fi.write(str(DESIRED_AUTO_VER))
 
-  def backup_reinstaller(self):     # Backup Reinstaller!
-    pass
+  def backup_reinstaller_loop(self):     # Backup Reinstaller!
+    backup_options = []
+
+    while 1:
+      self.selected_backup = get_user_backups()
+      if self.selected_backup is None:
+        print('Didn\'t select a backup, exiting.')
+        return
+      self.get_backup_options()
+      if self.backup_reinstall_function() == 'exit':
+        return
+  
+  def get_backup_options(self):  # Check what assets are available for the selected backup
+        
+    # Check if the selected backup has a boot logo asset
+    if os.path.exists('{}/{}/{}}'.format(BACKUPS_DIR, self.selected_backup, BOOT_LOGO_NAME)):
+      backup_options.append('Boot Logo')
+
+    # Check if the selected backup has a boot annimation asset
+    if os.path.exists('{}/{}/bootanimation.zip'.format(BACKUPS_DIR, self.selected_backup)):
+        backup_options.append('Boot Animation')
+
+    # Check if the selected backup has a OpenPilot Spinner asset
+    if os.path.exists('{}/{}/spinner'.format(BACKUPS_DIR, self.selected_backup)):
+      backup_options.append('OpenPilot Spinner')
+
+    # if os.path.exists('{}/{}/additional'.format(BACKUPS_DIR, self.selected_backup)):  # todo disabled for now
+    #   self.backup_options.append('4. Additional Resources')
+
+    backup_options.append('-Main Menu-')
+    backup_options.append('-Reboot-')
+    backup_options.append('Quit')
+
+  def backup_reinstall_function(self):       # Self installer program, prompts user on what they want to do
+    while 1:
+      options = list(backup_options)  # this only contains available options from self.get_available_options
+      if not len(options):
+        print('The selected backup has no resources available for your device! Try another.')
+        time.sleep(2)
+        return
+        
+      print('What resources do you want to install for the {} backup?'.format(self.selected_backup))
+      for idx, theme in enumerate(options):
+        print('{}. {}'.format(idx + 1, theme))
+      indexChoice = int(input("Enter Index Value: "))
+      indexChoice -= 1 
+
+      selected_option = backup_options[indexChoice]
+      print(selected_option)
+
+      if selected_option == 'Boot Logo':
+        print('Selected to install the Boot Logo backup. Continue?')
+        if not is_affirmative():
+          print('Not installing...')
+          time.sleep(1.5)
+          continue
+        os.system('cp {} {}'.format(BOOT_LOGO_PATH, self.backup_dir))  # Make Backup
+        os.system('dd if={}/{}/{} of={}'.format(BACKUPS_DIR, self.selected_backup, BOOT_LOGO_NAME, BOOT_LOGO_PATH))  # Replace
+        print('\nBoot Logo installed successfully! Original backed up to {}'.format(self.backup_dir))
+        print('Press enter to continue!')
+        input()
+      elif selected_option == 'Boot Animation':
+        print('Selected to install the Boot Animation backup. Continue?')
+        if not is_affirmative():
+          print('Not installing...')
+          time.sleep(1.5)
+          continue
+              
+                  os.system('mount -o remount,rw /system')  # /system read only, must mount as r/w
+                  os.system('mv /system/media/bootanimation.zip {}'.format(self.backup_dir))  # backup
+                  os.system('cp {}/{}/bootanimation.zip /system/media/bootanimation.zip'.format(BACKUPS_DIR, self.selected_backup))  # replace
+                  os.system('chmod 666 /system/media/bootanimation.zip')
+                  print('\nBoot Animation installed successfully! Original backed up to {}'.format(self.backup_dir))
+                  print('Press enter to continue!')
+                  input()
+      elif selected_option == 'OpenPilot Spinner':
+        print('Selected to install the OP Spinner backup. Continue?')
+        if not is_affirmative():
+          print('Not installing...')
+          time.sleep(1.5)
+          continue
+        print('Do you have an OP fork with a custom directory name? (ex. arnepilot, dragonpilot)')  # Ask the user if their OP fork used a diffrent directory.
+        if is_affirmative():  # Yes there is a custom OP dir
+          print('What is the OP directory name? (case matters, not including /data/)')
+          op_dir = '/data/{}'.format(input('> ').strip('/'))  # get custom dir name, strip slashes for safety
+          print('Your openpilot directory is {}'.format(op_dir))
+          input('*** Please enter to continue, or Ctrl+C to abort if this is incorrect! ***')
+
+          os.system('mv {}/selfdrive/ui/spinner/spinner {}'.format(op_dir, self.backup_dir))
+          os.system('cp {}/{}/spinner {}/selfdrive/ui/spinner'.format(BACKUPS_DIR, self.selected_backup, op_dir))
+          print('\n{} spinner installed successfully! Original backed up to {}'.format(op_dir.split('/')[2], self.backup_dir))
+        else:  # there is not custom OP dir
+          os.system('mv /data/openpilot/selfdrive/ui/spinner/spinner {}'.format(self.backup_dir))
+          os.system('cp {}/{}/spinner /data/openpilot/selfdrive/ui/spinner'.format(BACKUPS_DIR, self.selected_backup))
+          print('\nopenpilot spinner installed successfully! Original backed up to {}'.format(self.backup_dir))
+          print('Press enter to continue!')
+          input()
+      elif selected_option == 'Additional Resources':  # additional features
+        print('Additional Resources are not an active feature')
+        time.sleep(5)
+      elif selected_option == '-Main Menu-' or selected_option is None:
+        return
+      elif selected_option == '-Reboot-':
+        print('Rebooting.... Enjoy your new theme!!!')
+        os.system('am start -a android.intent.action.REBOOT')  # reboot intent is safer (reboot sometimes causes corruption)
+        return 'exit'
+
 
 if __name__ == '__main__':
   ti = ThemeInstaller()
