eb781dd89b37be74edca87994ab0798441869e22
Coltonton <41667601+Coltonton@users.noreply.github.com>
2021-08-22 01:43:14 -0400

    Update theme_install.py

--- theme_install.py
+++ theme_install.py
@@ -177,7 +177,7 @@ class ThemeInstaller:
                 if backup_overide_check(self.backup_dir, 'spinner') == True:
                     break
 
-                print(OP_LOC)
+                print("Location".format(OP_LOC))
                 install_from_path = ("{}/{}/spinner".format(CONTRIB_THEMES, self.selected_theme))
                 INSTALL_QT_SPINNER(self.backup_dir, OP_VER, OP_LOC, install_from_path, SHOW_CONSOLE_OUTPUT)
                 mark_self_installed()        # Create flag in /sdcard so auto installer knows there is a self installation
