bb7d1d669ba425183247fa7d648a8b2f0fad44d9
Coltonton <41667601+Coltonton@users.noreply.github.com>
2023-04-14 01:57:26 -0400

    idum x2

--- support/support_functions.py
+++ support/support_functions.py
@@ -234,8 +234,8 @@ def get_OP_Ver_Loc():           # Get OpenPilot Version & Location
 ##================= Installer Code =================== ##
 def INSTALL_BOOT_LOGO(DeviceData, backup_dir, install_from_path):
     #DebugPrint('Install_Boot_Logo boot logo path:{} boot logo name:{} backup_dir:{} install_from_path:{}'.format(DeviceData["BOOT_LOGO_PATH"], DeviceData["BOOT_LOGO_THEME_NAME"], backup_dir, install_from_path), overide="sf")
-    os.system('cp {} {}/{}'.format("BOOT_LOGO_PATH", backup_dir, DeviceData["BOOT_LOGO_THEME_NAME"]))    # Make Backup
-    os.system('dd if={} of={}'.format(install_from_path, "BOOT_LOGO_PATH"))           # Replace
+    os.system('cp {} {}/{}'.format(DeviceData["BOOT_LOGO_PATH"], backup_dir, DeviceData["BOOT_LOGO_THEME_NAME"]))    # Make Backup
+    os.system('dd if={} of={}'.format(install_from_path, DeviceData["BOOT_LOGO_PATH"]))           # Replace
     print('Boot Logo installed! Original file(s) backed up to {}'.format(backup_dir, DeviceData["BOOT_LOGO_THEME_NAME"]))
 
 def INSTALL_BOOTANIMATION(backup_dir, install_from_path, color=''):
