2b059ef163671227da4471ac00a91601c7b8ecda
Coltonton <41667601+Coltonton@users.noreply.github.com>
2023-04-16 05:19:44 -0400

    Revert "Merge branch 'Base-Dev' into pre-release"
    This reverts commit a005e1214c06b09314ccb58fb12fc8565b8bb5d5, reversing
    changes made to 6995b037ec0439ce36dc15b71348f08e988cdf23.

--- theme_utility.py
+++ theme_utility.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 ###################################################################################
-#                                  VER dev                                        #
+#                                  VER 1.2                                        #
  #                                                                                #
  #      Permission is granted to anyone to use this software for any purpose,     #
  #     excluding commercial applications, and to alter it and redistribute it     #
